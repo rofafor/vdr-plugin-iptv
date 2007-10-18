5b91ea05ebd0fc721264d22a734f1960da45ddcb
Antti Seppälä <a.seppala@gmail.com>
2007-10-18 20:18:33 +0000

    Exec doesn't need that much escaping. The url should be however wrapped in quotation marks because it may contain spaces.

--- iptv/iptvstream.sh
+++ iptv/iptvstream.sh
@@ -12,4 +12,4 @@ if [ $# -ne 1 ]; then
     exit 1;
 fi
 
-exec vlc ${URL} --sout \"#transcode{vcodec=mp2v,acodec=mpga,vb=800,ab=192}:standard{access=udp,mux=ts,dst=127.0.0.1:${1}}\" --intf dummy
+exec vlc "${URL}" --sout "#transcode{vcodec=mp2v,acodec=mpga,vb=800,ab=192}:standard{access=udp,mux=ts,dst=127.0.0.1:${1}}" --intf dummy
