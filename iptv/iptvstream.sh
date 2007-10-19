3e989d7050d09be8174309cb3621dd2c5ec1aa7a
Antti Seppälä <a.seppala@gmail.com>
2007-10-19 23:08:24 +0000

    Parametrize the bitrates for easier editing and management.

--- iptv/iptvstream.sh
+++ iptv/iptvstream.sh
@@ -11,6 +11,10 @@ PARAMETER=${1}
 # Iptv plugin listens this port
 PORT=${2}
 
+# Default bitrates for stream transcoding
+VBITRATE=2400
+ABITRATE=320
+
 # There is a way to specify multiple URLs in the same script. The selection is
 # then controlled by the extra parameter passed by IPTV plugin to the script
 case $PARAMETER in
@@ -39,4 +43,4 @@ let APID=${PARAMETER}+2
 let SPID=${PARAMETER}+3
 
 # Use 'exec' for capturing program pid for further management in IPTV plugin
-exec vlc "${URL}" --sout "#transcode{vcodec=mp2v,acodec=mpga,vb=2400,ab=320}:standard{access=udp,mux=ts{pid-video=${VPID},pid-audio=${APID},pid-spu=${SPID}},dst=127.0.0.1:${PORT}}" --intf dummy
+exec vlc "${URL}" --sout "#transcode{vcodec=mp2v,acodec=mpga,vb=${VBITRATE},ab=${ABITRATE}}:standard{access=udp,mux=ts{pid-video=${VPID},pid-audio=${APID},pid-spu=${SPID}},dst=127.0.0.1:${PORT}}" --intf dummy
