b3b0ae356713b2b1bac68462681e28f1ace1e526
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2007-11-04 17:07:25 +0000

    Incremented version number.

--- iptv.c
+++ iptv.c
@@ -3,7 +3,7 @@
  *
  * See the README file for copyright information and how to reach the author.
  *
- * $Id: iptv.c,v 1.28 2007/10/29 21:21:39 rahrenbe Exp $
+ * $Id: iptv.c,v 1.29 2007/11/04 17:07:25 rahrenbe Exp $
  */
 
 #include <getopt.h>
@@ -22,7 +22,7 @@
 #error "You must apply the closefilter patch for VDR!"
 #endif
 
-static const char VERSION[]     = "0.0.4";
+static const char VERSION[]     = "0.0.5";
 static const char DESCRIPTION[] = trNOOP("Experience the IPTV");
 
 class cPluginIptv : public cPlugin {
