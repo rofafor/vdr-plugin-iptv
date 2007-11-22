3daea8b1ba0bb9756c4b9987546b99d0fedbc184
Antti Seppälä <a.seppala@gmail.com>
2007-11-22 17:36:00 +0000

    Use consistent variable names.

--- iptv/iptvstream.sh
+++ iptv/iptvstream.sh
@@ -45,10 +45,10 @@ let SPID=${PARAMETER}+3
 # Capture program pid for further management in IPTV plugin
 vlc "${URL}" --sout "#transcode{vcodec=mp2v,acodec=mpga,vb=${VBITRATE},ab=${ABITRATE}}:standard{access=udp,mux=ts{pid-video=${VPID},pid-audio=${APID},pid-spu=${SPID}},dst=127.0.0.1:${PORT}}" --intf dummy &
 
-PID=$!
+PID=${!}
 
 trap 'kill -INT ${PID} 2> /dev/null' INT EXIT QUIT TERM
 
 # Waiting for the given PID to terminate
-wait $PID
+wait ${PID}
 
