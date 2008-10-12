04c194ffc78b87488c6d18a428862701eb537977
Rolf Ahrenberg <Rolf.Ahrenberg@sci.fi>
2008-10-12 23:08:41 +0300

    Incremented the version number.

--- iptv.c
+++ iptv.c
@@ -20,7 +20,7 @@
 #error "VDR-1.6.0 API version or greater is required!"
 #endif
 
-static const char VERSION[]     = "0.2.1";
+static const char VERSION[]     = "0.2.2";
 static const char DESCRIPTION[] = trNOOP("Experience the IPTV");
 
 class cPluginIptv : public cPlugin {
